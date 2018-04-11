fddde0a29f1a3cd2297170b46971df06a83e6051
BananaBb <samson8682@gmail.com>
2018-04-11 19:05:29 +0800

    UPT: ui change

--- Controller/Component/QueryLogComponent.php
+++ Controller/Component/QueryLogComponent.php
@@ -2,23 +2,32 @@
 
 class QueryLogComponent extends Component {
 
-    public function printQuery($model)
+    public function printQuery($model = '', $searchTable = '')
     {
         if (empty($model)) {
-            print_r('Model not exist');
+            print("<pre>".print_r(['Err' => 'Model not exist'], true)."</pre>");
             return;
         }
-        $this->convert($model->getDataSource()->getLog(false, false));
+        $log = $model->getDataSource()->getLog(false, false)['log'] ?: [];
+        if (!empty($searchTable)) {
+            foreach($log as $key => $val) {
+                if (!empty($val['query']) && preg_match("/".strtolower($searchTable)."/", strtolower($val['query']))) {
+                    continue;
+                }
+                unset($log[$key]);
+            }
+        }
+        $this->convert($log);
     }
 
     public function convert($queryArr = null)
     {
-        if (empty($queryArr) && !is_array($queryArr)) {
+        if (empty($queryArr) || !is_array($queryArr)) {
             return [];
         }
         
         $transform = json_encode($queryArr);
-        print_r(json_decode($transform), true);
+        print("<pre>".print_r(json_decode($transform), true)."</pre>");
         return;
     }
 }
